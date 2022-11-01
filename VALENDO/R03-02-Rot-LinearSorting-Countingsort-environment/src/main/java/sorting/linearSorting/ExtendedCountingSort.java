a9bdcd7001748f8903659c5910e6fe8dc88dc0bb
vitor barbosa <jose.vitor.maciel@ccc.ufcg.edu.br>
2022-11-01 01:07:06 -0300

    [ADD] ExtendedCount*

---
+++ VALENDO/R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -0,0 +1,56 @@
+package sorting.linearSorting;
+
+import sorting.AbstractSorting;
+
+/**
+ * Classe que implementa do Counting Sort vista em sala. Desta vez este
+ * algoritmo deve satisfazer os seguintes requisitos:
+ * - Alocar o tamanho minimo possivel para o array de contadores (C)
+ * - Ser capaz de ordenar arrays contendo numeros negativos
+ */
+public class ExtendedCountingSort extends AbstractSorting<Integer> {
+
+	@Override
+	public void sort(Integer[] array, int leftIndex, int rightIndex) {
+
+		int max = 0;
+		int min = 0;
+
+		int[] sorted = new int[array.length];
+
+		// 0º Passo: define o tamanho do array auxiliar com base
+		// no maior valor presente
+		for (int l = leftIndex; l <= rightIndex; l++) {
+			if (array[l] > max) {
+				max = array[l];
+			} else {
+				min = array[l];
+			}
+		}
+		max += 1;
+
+		int[] aux = new int[max];
+
+		// 1º Passo: contagem da frequência do array;
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			aux[array[i]] += 1;
+		}
+		aux[0] += leftIndex;
+
+		// 2º Passo: acumulativa do array;
+		for (int j = 1; j < aux.length; j++) {
+			aux[j] = aux[j] + aux[j - 1];
+		}
+
+		// 3º Passo: repasse ordenado para novo array;
+		for (int k = rightIndex; k >= leftIndex; k--) {
+			sorted[aux[array[k]] - 1] = array[k];
+			aux[array[k]] -= 1;
+		}
+
+		for (int z = leftIndex; z <= rightIndex; z++) {
+			array[z] = sorted[z];
+		}
+	}
+
+}
