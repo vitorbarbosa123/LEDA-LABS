11d9e12d670245a3717f9b2f4039d3a558c3ec5e
vitor barbosa <vitorbarbosa789@gmail.com>
2022-12-01 23:00:05 -0300

    [ADD] openAddress to impl

--- VALENDO/R07-02-Rot-TabelaHash-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ VALENDO/R07-02-Rot-TabelaHash-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -2,7 +2,6 @@ package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
-import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 
 public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 		AbstractHashtableOpenAddress<T> {
@@ -14,92 +13,28 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 		this.initiateInternalTable(size);
 	}
 
-	private int getIndex(T element, int probe) {
-		return ((HashFunctionOpenAddress) this.hashFunction).hash(element, probe);
-	}
-
 	@Override
 	public void insert(T element) {
-		if(isFull()) {
-			throw new HashtableOverflowException();
-		}
-		else {
-			int probe = 0;
-			int index = getIndex(element, probe);
-			if(element != null) {
-				while( this.table[index] != null && 
-				       this.table[index] != this.deletedElement && 
-					   probe < this.table.length) {
-							probe++;
-							index = getIndex(element, probe);
-							this.COLLISIONS++;
-					   }
-				this.table[index] = element;
-				this.elements++;
-			}
-		}
-		
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
 	}
 
 	@Override
 	public void remove(T element) {
-		if(!isEmpty()) {
-			int probe = 0;
-			int index = getIndex(element, probe);
-			if(element != null) {
-				while(this.table[index] != null &&
-					  this.table[index] != this.deletedElement &&
-					  !this.table[index].equals(element) &&
-					  probe < this.table.length	) {
-						probe++;
-						index = getIndex(element, probe);
-					  }
-				if(this.table[index] != null && 
-				   this.table[index].equals(element)) {
-					this.table[index] = this.deletedElement;
-					this.elements--;
-				   }
-			}
-		}
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
 	}
 
 	@Override
 	public T search(T element) {
-		T isElementInTable = null;
-		int isElementOut = indexOf(element);
-
-		if(!isEmpty()) {
-			if(element != null && isElementOut > 0) {
-				if(this.table[isElementOut] instanceof DELETED) {
-					isElementInTable = null;
-				} else {
-					isElementInTable = (T) this.table[isElementOut];
-				}
-
-			   }
-		}
-		return isElementInTable;
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
 	}
 
 	@Override
 	public int indexOf(T element) {
-		int isElementInTable = -1;
-		if(!isEmpty()) {
-			int probe = 0;
-			int index = getIndex(element, probe);
-			if(element != null) {
-				while(this.table[index] != null && 
-				      this.table[index] != this.deletedElement &&
-					  !this.table[index].equals(element) && probe < this.table.length) {
-						probe++;
-						index = getIndex(element, probe);
-				}
-				if(probe < this.table.length) {
-					isElementInTable = index;
-				}
-			}
-		}
-		return isElementInTable;
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
 	}
 
 }
