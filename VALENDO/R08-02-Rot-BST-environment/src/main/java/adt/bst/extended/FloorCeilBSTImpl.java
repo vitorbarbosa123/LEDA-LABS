5a0e6cee638da8b485acd2916405886a40675912
vitor barbosa <vitorbarbosa789@gmail.com>
2022-12-08 11:55:30 -0300

    [ADD] impl flor and cell to bst

--- VALENDO/R08-02-Rot-BST-environment/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
+++ VALENDO/R08-02-Rot-BST-environment/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
@@ -11,14 +11,43 @@ public class FloorCeilBSTImpl extends BSTImpl<Integer> implements FloorCeilBST {
 
 	@Override
 	public Integer floor(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return floor(array, numero, 0, (Integer) null);
+	}
+
+	private Integer floor(Integer[] array, double numero, Integer element, int index) {
+		if(index < array.length) {
+			this.insert(array[index]);
+			Integer possibleFloor = array[index];
+
+			if(possibleFloor == numero || 
+			  (element == null && possibleFloor < numero) ||
+			  (element != null && possibleFloor < numero && element < possibleFloor)) {
+				element = possibleFloor;
+			}
+			return floor(array, numero, index++, element);
+		}
+		return element;
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return ceil(array, numero, 0, (Integer) null);
+		
+	}
+
+	private Integer ceil(Integer[] array, double numero, Integer element, int index) {
+		if(index > array.length) {
+			this.insert(array[index]);
+			Integer possibleCeil = array[index];
+
+			if(possibleCeil == numero || 
+			  (element == null && possibleCeil > numero) ||
+			  (element != null && possibleCeil > numero && element > possibleCeil)) {
+				element = possibleCeil;
+			}
+			return floor(array, numero, index++, element);
+		}
+		return element;
 	}
 
 }
