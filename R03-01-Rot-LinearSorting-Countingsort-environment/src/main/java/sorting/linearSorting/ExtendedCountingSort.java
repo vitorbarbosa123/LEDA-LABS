8cb0416bcc05a039990dfc1b7732e083414a235e
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-24 10:40:31 -0300

    [ADD] extended counting

---
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -0,0 +1,66 @@
+package sorting.linearSorting;
+
+import java.util.Arrays;
+
+import sorting.AbstractSorting;
+
+/**
+ * Classe que implementa do Counting Sort vista em sala. Desta vez este
+ * algoritmo deve satisfazer os seguitnes requisitos:
+ * - Alocar o tamanho minimo possivel para o array de contadores (C)
+ * - Ser capaz de ordenar arrays contendo numeros negativos
+ */
+public class ExtendedCountingSort extends AbstractSorting<Integer> {
+
+	@Override
+	public void sort(Integer[] array, int leftIndex, int rightIndex) {
+		if(array.length > 1) {
+			couting(array, leftIndex, rightIndex);
+		}
+	}
+
+	private Integer[] couting(Integer[] array, int leftIndex, int rightIndex) {
+		
+		// Descobrindo o maior e o menor valor do array
+		int maior = array[rightIndex];
+		int menor = array[leftIndex];
+
+		for(int i = leftIndex; i < rightIndex - 1; i++) {
+			if(maior < array[i]) {
+				maior = array[i];
+			}
+			if(array[i] < menor) {
+				menor = array[i];
+			}
+		}
+
+		// Criando um array de frequencia com base no maior e menor valor
+		Integer[] frequence = new Integer[maior - menor + 1];
+
+		// Setando todos os valores do novo array para zero
+		for(int j = 0; j < frequence.length; j++) {
+			frequence[j] = 0;
+		}
+
+		// Calculo de frequencia
+		for(int m = leftIndex; m <= rightIndex; m++) {
+			frequence[array[m] - menor] += 1;
+		}
+
+		// Acumulativa em array de frequência
+		for(int n = 1; n < frequence.length; n++) {
+			frequence[n] += frequence[n - 1];
+		}
+
+		// Criando array do tamanho do array original
+		Integer[] result = new Integer[array.length];
+		 for(int p = rightIndex; p >= leftIndex; p--) {
+			result[frequence[array[p] - menor] - 1] = array[p];
+	
+			frequence[array[p] - menor] -= 1;
+		}
+
+		return result;
+ 	}
+
+}
