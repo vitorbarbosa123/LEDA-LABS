f36693b4e0ca18d3a383952e584b6e376fe69198
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-24 10:58:44 -0300

    [ADD] removing menor

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -19,27 +19,22 @@ public class CountingSort extends AbstractSorting<Integer> {
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
 		if(array.length > 1) {
 			couting(array, leftIndex, rightIndex);
-		}
-		
+		}	
 	}
 
 	private Integer[] couting(Integer[] array, int leftIndex, int rightIndex) {
 		
-		// Descobrindo o maior e o menor valor do array
+		// Descobrindo o maior valor do array
 		int maior = array[rightIndex];
-		int menor = array[leftIndex];
 
 		for(int i = leftIndex; i < rightIndex - 1; i++) {
 			if(maior < array[i]) {
 				maior = array[i];
 			}
-			if(array[i] < menor) {
-				menor = array[i];
-			}
 		}
 
-		// Criando um array de frequencia com base no maior e menor valor
-		Integer[] frequence = new Integer[maior - menor + 1];
+		// Criando um array de frequencia com base no maior valor
+		Integer[] frequence = new Integer[maior];
 
 		// Setando todos os valores do novo array para zero
 		for(int j = 0; j < frequence.length; j++) {
@@ -48,7 +43,7 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 		// Calculo de frequencia
 		for(int m = leftIndex; m <= rightIndex; m++) {
-			frequence[array[m] - menor] += 1;
+			frequence[array[m] - 1] += 1;
 		}
 
 		// Acumulativa em array de frequência
@@ -59,9 +54,9 @@ public class CountingSort extends AbstractSorting<Integer> {
 		// Criando array do tamanho do array original
 		Integer[] result = new Integer[array.length];
 		 for(int p = rightIndex; p >= leftIndex; p--) {
-			result[frequence[array[p] - menor] - 1] = array[p];
+			result[frequence[array[p] - 1] - 1] = array[p];
 	
-			frequence[array[p] - menor] -= 1;
+			frequence[array[p] - 1] -= 1;
 		}
 
 		return result;
