b1186203decdce29be8657082a10b4fd9a5b21b6
vitor barbosa <vitorbarbosa789@gmail.com>
2023-05-05 10:54:36 -0300

    [ADD] simple queue, stack impl

---
+++ R05-01-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueImpl.java
@@ -0,0 +1,67 @@
+package adt.queue;
+
+import java.util.Arrays;
+
+public class QueueImpl<T> implements Queue<T> {
+
+	private T[] array;
+	private int tail;
+	private int head;
+
+	@SuppressWarnings("unchecked")
+	public QueueImpl(int size) {
+		array = (T[]) new Object[size];
+		tail = -1;
+		head = -1;
+	}
+
+	@Override
+	public T head() {
+		if(!isEmpty()) {
+			return this.array[head];
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.tail == -1;
+	}
+
+	@Override
+	public boolean isFull() {
+		return this.tail == array.length;
+	}
+
+	private void shiftLeft() {
+		for(int i = 0; i < tail; i++) {
+			array[i] = array[i+1];
+		}
+	}
+
+	@Override
+	public void enqueue(T element) throws QueueOverflowException {
+		if(!isFull()) {
+			if(this.head() == null) {
+				head++;
+				System.out.println(array[head]);
+				array[head] = element;
+				tail++;
+			} else {
+				tail++;
+				array[tail] = element;
+			}
+		}
+	}
+
+	@Override
+	public T dequeue() throws QueueUnderflowException {
+		T item = this.head();
+		if(!isEmpty()) {
+			shiftLeft();
+			tail--;
+		}
+		return item;
+	}
+
+}
---
+++ R05-01-Rot-TAD-Linear-environment/src/main/java/adt/stack/StackImpl.java
@@ -0,0 +1,48 @@
+package adt.stack;
+
+public class StackImpl<T> implements Stack<T> {
+
+	private T[] array;
+	private int top;
+
+	@SuppressWarnings("unchecked")
+	public StackImpl(int size) {
+		array = (T[]) new Object[size];
+		top = -1;
+	}
+
+	@Override
+	public T top() {
+		return array[top];                                                   
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.top == -1;
+	}
+
+	@Override
+	public boolean isFull() {
+		return this.top == array.length - 1;
+	}
+
+	@Override
+	public void push(T element) throws StackOverflowException {
+		if(!isFull()) {
+			top++;
+			array[top] = element;
+		}
+	}
+
+	@Override
+	public T pop() throws StackUnderflowException {
+		T item = null;
+		if(!isEmpty()) {
+			item = this.top();
+
+		}
+		return item;
+		
+	}
+
+}
