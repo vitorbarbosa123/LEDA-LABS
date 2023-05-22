607edc0bf5250a1e7d0f62c13c260a4874d44940
vitor barbosa <vitorbarbosa789@gmail.com>
2023-05-22 20:05:03 -0300

    [ADD] openHashtable + test

---
+++ R07-01-Rot-TabelaHash-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -0,0 +1,104 @@
+package adt.hashtable.open;
+
+import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
+import adt.hashtable.hashfunction.HashFunctionOpenAddress;
+import adt.hashtable.hashfunction.HashFunctionQuadraticProbing;
+
+public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
+		extends AbstractHashtableOpenAddress<T> {
+
+	public HashtableOpenAddressQuadraticProbingImpl(int size,
+			HashFunctionClosedAddressMethod method, int c1, int c2) {
+		super(size);
+		hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
+		this.initiateInternalTable(size);
+	}
+
+	@Override
+	public void insert(T element) {
+		if(isFull()) {
+			throw new HashtableOverflowException();
+		}
+
+		if(element != null) {
+			int count = 0;
+			boolean aux = true;
+			int index = -1;
+
+			while(aux && count < this.capacity()) {
+	
+				index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, count++);
+				
+				if(this.table[index] == null || this.table[index].equals(new DELETED())) {
+					this.table[index] = element;
+					elements++;
+					aux = false;
+				} else {
+					COLLISIONS++;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void remove(T element) {
+		if(element != null) {
+			int count = 0;
+			boolean aux = true;
+			int index;
+
+			while(aux && count < capacity()) {
+	
+				index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, count++);
+				
+				if(this.table[index] != null && this.table[index].equals(element)) {
+					this.table[index] = new DELETED();
+					elements--;
+					aux = false;
+				}
+			}
+		}
+	}
+
+	@Override
+	public T search(T element) {
+		T result = null;
+
+		if(element != null) {
+			int count = 0;
+			boolean aux = true;
+			int index = 0;
+
+			while(aux && count < this.capacity()) {
+	
+				index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, count++);
+				
+				if(this.table[index] != null && this.table[index].equals(element)) {
+					result = (T) this.table[index];
+					aux = false;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	@Override
+	public int indexOf(T element) {
+		int result = -1;
+		int count = 0;
+		boolean aux = true;
+		int index = 0;
+
+		while(aux && count < capacity()) {
+			index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, count++);
+
+			if(this.table[index] != null && this.table[index].equals(element)) {
+				result = index;
+				aux = false;
+			}
+
+		}
+		return result;
+	}
+}
